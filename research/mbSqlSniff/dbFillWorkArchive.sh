72aba8a968bc4ff8aaa14c3a379b72b97dc2950f
adsieben <adsieben@gmail.com>
2024-02-08 16:14:27 +0100

    research/mbSqlSniff/dbFillWorkArchive.sh added

---
+++ research/mbSqlSniff/dbFillWorkArchive.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#https://stackoverflow.com/a/30518622
+#Pathname Expansion — After word splitting, *unless the -f option has been set,
+#bash scans each word for the characters *, ?, and [. If one of these characters 
+#appears, then the word is regarded as a pattern, and replaced with an 
+#alphabetically sorted list of file names matching the pattern.
+
+sqlite3 values.20240127.sqlite << \EOF
+ATTACH DATABASE "values.sqlite" AS srcDB;
+insert into tPvSec select * from srcDB.tPvSec where fdatetime > (select max( fdatetime ) from tPvSec );
+insert into tPvTenSec select * from srcDB.tPvTenSec where fdatetime > (select max( fdatetime ) from tPvTenSec );
+delete  from srcDB.tpvsec where srcdb.tpvsec.fdatetime < (select CAST(strftime('%s', 'now', '-24 hour') as INTEGER) as time) ;
+delete  from srcDB.tpvTensec where srcdb.tpvTensec.fdatetime < (select CAST(strftime('%s', 'now', '-24 hour') as INTEGER) as time) ;
+EOF
+
+
+#turn of bash expansion of "*"
+# set -f
+# SQL=$(cat << \EOF
+# ATTACH DATABASE "values.sqlite" AS srcDB;
+# insert into tPvSec select * from srcDB.tPvSec where fdatetime > (select max( fdatetime ) from tPvSec );
+# insert into tPvTenSec select * from srcDB.tPvTenSec where fdatetime > (select max( fdatetime ) from tPvTenSec );
+# delete  from srcDB.tpvsec where srcdb.tpvsec.fdatetime < (select CAST(strftime('%s', 'now', '-24 hour') as INTEGER) as time) ;
+# delete  from srcDB.tpvTensec where srcdb.tpvTensec.fdatetime < (select CAST(strftime('%s', 'now', '-24 hour') as INTEGER) as time) ;
+# EOF
+# )
+
+#no pathname expansion when variable in "" 
+# echo "$SQL"
+
+
+#no pathname expansion when variable in with set -f 
+# set -f
+# echo $SQL
+# set +f
+# echo $SQL
+
+
